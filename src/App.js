7eb0a5dca9adb7db5bf663de4e220de4b1f0fb97
AC/Dev <angelocardona85@gmail.com>
2019-06-11 22:48:38 -0400

    end first part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <div className="App">
-    
+      <Movies />
     </div>
   );
 }
